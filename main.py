426d0e4138ed4377681514cbe8a472da5efe2139
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-12-28 14:18:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
 
+import pandas as pd
+import seaborn as sns
+y = parkinsons[ 'status']
+x = parkinsons[ ['PPE', 'DFA']]
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+x_train_scaled = scaler.fit_transform(x)
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors= 1)
+model.fit(x_train, y_train)
+from sklearn.metrics import accuracy_score
+y_predict = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_predict)
+print(accuracy)
